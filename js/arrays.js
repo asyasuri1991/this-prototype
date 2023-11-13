ae39ddad8bcbbd9e8ca4748a3db420edffbd1a8b
asyasuri1991 <asyasuri@gmail.com>
2023-11-13 16:01:33 +0300

    решила задачу 2(ветка 2ишью)

--- js/arrays.js
+++ js/arrays.js
@@ -1,11 +1,40 @@
 function task1() {
     function createLinkProduct(category) {
-return function (article) {
-    return `https://myshop.ru/${category}/${article}`;
-}
+        return function (article) {
+            return `https://myshop.ru/${category}/${article}`;
+        }
     }
-    createPCLink = createLinkProduct ('pc');
-    createMonitorLink = createLinkProduct ('monitors');
-    createNotebookLink = createLinkProduct ('notebooks');
+    createPCLink = createLinkProduct('pc');
+    createMonitorLink = createLinkProduct('monitors');
+    createNotebookLink = createLinkProduct('notebooks');
     console.log(createPCLink(100));
+}
+
+
+function task2() {
+    let orders = [
+        {
+            fio: "Ivan",
+            tovar: "phone",
+            price: 10000,
+            quantity: 2
+        },
+        {
+            fio: "Peter",
+            tovar: "usb",
+            price:2300,
+            quantity:5
+        },
+        {
+            fio: "Mary",
+            tovar: "connector",
+            price:100,
+            quantity:100
+        },
+    ]
+    function showOrder(){
+        let str = `Заказ покупателя ${this.fio} составил ${this.price*this.quantity}`;
+        console.log(str);
+    }
+    orders.forEach(elem=>showOrder.call(elem));
 }
